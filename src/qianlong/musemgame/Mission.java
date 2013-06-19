92f522b9bc091e2b27993aa68be6bd74fcbe4185
superchild <flyseed426@gmail.com>
2013-06-19 23:48:45 +0800

    Modify Mission and Question class

--- src/qianlong/musemgame/Mission.java
+++ src/qianlong/musemgame/Mission.java
@@ -1,5 +1,16 @@
 package qianlong.musemgame;
 
-public class Mission {
+import java.util.ArrayList;
 
+public class Mission {
+	ArrayList<Question> question;	//question of mission
+	int	score;						//total score of mission
+	
+	private static final int QUESTION_NUM = 5; 
+	//contructor
+	public Mission()
+	{
+		this.question = new ArrayList<Question>(QUESTION_NUM);
+		this.score = 0;
+	}
 }
--- src/qianlong/musemgame/Question.java
+++ src/qianlong/musemgame/Question.java
@@ -7,17 +7,19 @@ public class Question {
 	int answer;		 	//	correct answer id
 	int	choice;		 	//	user's answer choice
 	String question; 	//	question string
-	ArrayList<String> candidate = new ArrayList<String>(3);	//answer candidate string
+	ArrayList<String> candidate;	//answer candidate string
 	Antique antique;
 
 	public static final int CHOICE_A = 1;
 	public static final int CHOICE_B = 2;
 	public static final int CHOICE_C = 3;
 
+	private static final int CANDIDATE_NUM = 3;
 	//constructor
 	public Question(){
 		this.answer = 0;
 		this.choice = 0;
 		this.antique = new Antique();
+		this.candidate = new ArrayList<String>(CANDIDATE_NUM);
 	}
 }
