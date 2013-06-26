4e6cff8d628f3b538a525c5b6384dd37f400b1e0
superchild <flyseed426@gmail.com>
2013-06-26 22:48:38 +0800

    modify gameview state engine

--- src/qianlong/musemgame/GameView.java
+++ src/qianlong/musemgame/GameView.java
@@ -2,26 +2,22 @@ package qianlong.musemgame;
 
 import java.util.ArrayList;
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.BitmapFactory;
+import android.annotation.SuppressLint;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+@SuppressLint("ViewConstructor")
 public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 	int missionID;
 	int questionID;		// record the question number 
 	int status;			// record the game progress 
 	int timeCounter;	// current time counter 
 	DrawThread dt;		// draw screen thread 
-	ArrayList<Question> allQuestion = new ArrayList<Question>(5);
 	ArrayList<Rect> allCandidate = new ArrayList<Rect>(3);
 	Mission mission;	// mission
-	Rect [] rectCandidate;
-	//Rect rectQuestion;
-	//Rect rectAntique;
+	Question current;	// current question
 	
 	// game progress constant 
 	private static final int BEFORE_QUESTION = 1;
@@ -32,15 +28,18 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 	private static final int QUESTION_NUMBER = 5;
 	
 	//expire time between game status
-	private static final int WAIT_BEFORE_QUESTION = 1;
-	private static final int WAIT_AFTER_QUESTION = 1;
-	private static final int WAIT_USER_CHOOSE = 5;
+	private static final int WAIT_BEFORE_QUESTION = 10;
+	private static final int WAIT_AFTER_QUESTION = 10;
+	private static final int WAIT_USER_CHOOSE = 30;
 	public GameView(CreativeMusem father, int missionID) {
 		super(father);				
 		getHolder().addCallback(this);
 		mission = new Mission(missionID);
 		mission.initMission(father);			
 		
+		initRects();
+		
+		current = mission.question.get(questionID);
 		/* init game settings */
 		this.missionID = missionID;
 		this.questionID = 1;
@@ -61,65 +60,84 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 	
 	/* screen drawing method */
 	protected void doDraw(Canvas canvas){
+		
 		switch(status){
 			case BEFORE_QUESTION:
-				// draw question number animation 
-				//drawQuestionID();
-				
+				canvas.drawBitmap(current.antique.bmpAntique, 30, 30, null);
+				timeCounter++;
+				if(timeCounter == WAIT_BEFORE_QUESTION){
+					status = BEGIN_QUESTION;
+					timeCounter = 0;
+				}
 				break;
-			case BEGIN_QUESTION:
-				// draw question and candidate 
-				//
+			case BEGIN_QUESTION:		
+				canvas.drawBitmap(current.antique.bmpAntique, 30, 30, null);
+				canvas.drawText(current.question, 30, 60, null);        //drawQuestionID();
+				canvas.drawText(current.candidate.get(Question.CHOICE_A), 60, 30, null);
+				canvas.drawText(current.candidate.get(Question.CHOICE_B), 60, 60, null);
+				canvas.drawText(current.candidate.get(Question.CHOICE_C), 60, 90, null);
+				timeCounter++;
+				if(current.choice != 0){
+					status = AFTER_QUESTION;
+					timeCounter = 0;
+				}
 				
+				if(timeCounter == WAIT_USER_CHOOSE){
+					status = AFTER_QUESTION;
+					timeCounter = 0;
+					current.choice = Question.CHOICE_A;
+				}
+					
 				break;
 			case AFTER_QUESTION:
-				if(questionID == QUESTION_NUMBER){
-					
-				}
-				else{
-					questionID++;
-					status = BEFORE_QUESTION;
-				}
-					
+				canvas.drawBitmap(current.antique.bmpAntique, 30, 30, null);
+				canvas.drawText(current.question,30,60,null);        //drawQuestionID();
+				//this should be drawed according to answer's position
+				canvas.drawText(current.candidate.get(current.choice), 60, 30, null);
+				timeCounter++;
+				if(timeCounter == WAIT_AFTER_QUESTION){
+					timeCounter = 0;
+					if(questionID == QUESTION_NUMBER){
+						dt.isGameOn = false;
+					}
+					else{
+						questionID++;
+						current = mission.question.get(questionID);
+						status = BEFORE_QUESTION;
+					}
+				}	
 				break;
 		}
-		/* draw question animation */
-		
-		/* draw question */
-		
-		/* draw antique object */
 	}
 	
 	//initialize touch rectangular range 
 	public void initRects(){
-		rectCandidate = new Rect[3];
-    	for(int i=0;i<3;i++){
-    		rectCandidate[i] = new Rect(244,200+40*i,280,236+40*i);
-    	}
-    	//rectQuestion = new Rect();
-    	//rectAntique = new Rect();
-    	
+		Rect rectA = new Rect(224, 240, 280, 276);
+		Rect rectB = new Rect(224, 280, 280, 316);
+		Rect rectC = new Rect(224, 320, 280, 356);
+
+		allCandidate.add(Question.CHOICE_A, rectA);
+		allCandidate.add(Question.CHOICE_B, rectB);
+		allCandidate.add(Question.CHOICE_C, rectC); 	
     }
 	
 	// Handle onTouchEvent in main activity
 	public void touchEvent(int x, int y){
-		
 		// user choose answer A
-		if(rectCandidate[0].contains(x, y)){
-			allQuestion.get(questionID).choice = Question.CHOICE_A;
+		if(allCandidate.get(Question.CHOICE_A).contains(x, y)){
+			current.choice = Question.CHOICE_A;
 		}
 		
 		// user choose answer B
-		if(rectCandidate[1].contains(x, y)){
-			allQuestion.get(questionID).choice = Question.CHOICE_B;
+		if(allCandidate.get(Question.CHOICE_B).contains(x, y)){
+			current.choice = Question.CHOICE_B;
 		}
 		
 		// user choose answer C
-		if(rectCandidate[2].contains(x, y)){
-			allQuestion.get(questionID).choice = Question.CHOICE_C;
+		if(allCandidate.get(Question.CHOICE_C).contains(x, y)){
+			current.choice = Question.CHOICE_C;
 		}
 	}
-	
 	
 	@Override
 	protected void finalize() throws Throwable {
