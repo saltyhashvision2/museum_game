7c0b26b86197a0d7ee23927ccafc91ff57f651cf
zrlightning <zrlightning@gmail.com>
2013-06-27 23:20:03 +0800

    fix:click on button is inaccurate to trigger the ontouch event

--- src/qianlong/musemgame/CreativeMusem.java
+++ src/qianlong/musemgame/CreativeMusem.java
@@ -1,5 +1,7 @@
 package qianlong.musemgame;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Rect;
@@ -14,29 +16,29 @@ public class CreativeMusem extends Activity {
 	GameView gv;		//game view
 	MainView mainview;  //main view
 	View     current;	//current view
-	Rect rectStart;				//開始按鈕的矩形框
-	Rect rectQuit;				//退出按鈕的矩形框
-	Rect rectConfig;			//設定按鈕的矩形框
+	Bitmap start;
+	Bitmap quit;
+	Bitmap config;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);	//設定全螢幕
-        //getWindow().setFlags(
-        //		WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        //		WindowManager.LayoutParams.FLAG_FULLSCREEN
-        //		);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);	//設定全螢幕
+        getWindow().setFlags(
+        		WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        		WindowManager.LayoutParams.FLAG_FULLSCREEN
+        		);
 		mainview = new MainView(this);
 		//gv = new GameView(this,1);
 		setContentView(mainview);
 		current = mainview;
-        initRects();		//初始化用於匹配點擊事件的矩形框
+		initBitmap();		//初始化用於匹配點擊事件的矩形框
 	}
     //方法：初始化矩形框
-    public void initRects(){
-    	rectStart = new Rect(205,425,295,475);//pixel 90*50
-    	rectQuit = new Rect(205,525,295,575);//pixel 90*50
-    	rectConfig = new Rect(25,425,61,461);//pixel 36*36
+    public void initBitmap(){
+    	start = BitmapFactory.decodeResource(getResources(), R.drawable.start);
+    	quit = BitmapFactory.decodeResource(getResources(), R.drawable.quit);
+    	config = BitmapFactory.decodeResource(getResources(), R.drawable.config);
     }
     
 	@Override
@@ -45,13 +47,16 @@ public class CreativeMusem extends Activity {
 			int x = (int)event.getX();		//get x-coordinate of touch
 			int y = (int)event.getY();		//get y-coordinate of touch				
 			if(current == mainview){//如果目前介面是歡迎介面
-				if(rectStart.contains(x, y)){
+				if(x > 205 && x < start.getWidth()+205 &&
+						y > 425 && y < start.getHeight()+425 ){
 					Log.d("TAG", "This is rectStart!");
-				}else if(rectConfig.contains(x, y)){
-					Log.d("TAG", "This is rectConfig!");
-				}else if(rectQuit.contains(x, y)){		//按下退出鍵
+				}else if(x > 205 && x < quit.getWidth()+205 &&
+						y > 525 && y < start.getHeight()+525 ){
 					Log.d("TAG", "This is rectQuit!");
-					System.exit(0);						//程式退出
+					//System.exit(0);
+				}else if(x > 25 && x < config.getWidth()+25 &&
+						y > 425 && y < config.getHeight()+425 ){
+					Log.d("TAG", "This is rectConfig!");
 				}
 			}
 			else if(current == gv){
